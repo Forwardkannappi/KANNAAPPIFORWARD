81ea88104bd320eab32f8ddde00a3d785b475a3b
Forwardkannappi <116616204+Forwardkannappi@users.noreply.github.com>
2022-10-25 09:09:53 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,100 @@
+import re
+import base64
+import logging
+from struct import pack
+from pyrogram.file_id import FileId
+from pymongo.errors import DuplicateKeyError
+from umongo import Instance, Document, fields
+from motor.motor_asyncio import AsyncIOMotorClient
+from marshmallow.exceptions import ValidationError
+from config import Config
+DATABASE_URI, DATABASE_NAME, COLLECTION_NAME = Config.DATABASE_URI, Config.DATABASE_NAME, Config.COLLECTION_NAME
+
+client = AsyncIOMotorClient(DATABASE_URI)
+db = client[DATABASE_NAME]
+instance = Instance(db)
+
+
+@instance.register
+class Data(Document):
+    id = fields.StrField(attribute='_id')
+    channel = fields.StrField()
+    file_type = fields.StrField()
+    message_id = fields.IntField()
+    use = fields.StrField()
+    methord = fields.StrField()
+    caption = fields.StrField()
+
+    class Meta:
+        collection_name = COLLECTION_NAME
+
+async def save_data(id, channel, message_id, methord, caption, file_type):
+    file_id, file_ref = unpack_new_file_id(id)
+    try:
+        data = Data(
+            id=file_id,
+            use = "forward",
+            channel=channel,
+            message_id=message_id,
+            methord=methord,
+            caption=caption,
+            file_type=file_type
+        )
+    except ValidationError:
+        print('Error occurred while saving file in database')
+        return False, 2
+    try:
+        await data.commit()
+    except DuplicateKeyError:
+        print("Already saved in Database")
+        return False, 0
+    else:
+        try:
+            print("Messsage saved in DB")
+            return True, 1
+        except:
+            pass
+
+async def get_search_results():
+    filter = {'use': "forward"}
+    cursor = Data.find(filter)
+    cursor.sort('$natural', -1)
+    cursor.skip(0).limit(1)
+    Messages = await cursor.to_list(length=1)
+    return Messages
+
+
+def encode_file_id(s: bytes) -> str:
+    r = b""
+    n = 0
+
+    for i in s + bytes([22]) + bytes([4]):
+        if i == 0:
+            n += 1
+        else:
+            if n:
+                r += b"\x00" + bytes([n])
+                n = 0
+
+            r += bytes([i])
+
+    return base64.urlsafe_b64encode(r).decode().rstrip("=")
+
+
+def encode_file_ref(file_ref: bytes) -> str:
+    return base64.urlsafe_b64encode(file_ref).decode().rstrip("=")
+
+def unpack_new_file_id(new_file_id):
+    """Return file_id, file_ref"""
+    decoded = FileId.decode(new_file_id)
+    file_id = encode_file_id(
+        pack(
+            "<iiqq",
+            int(decoded.file_type),
+            decoded.dc_id,
+            decoded.media_id,
+            decoded.access_hash
+        )
+    )
+    file_ref = encode_file_ref(decoded.file_reference)
+    return file_id, file_ref
